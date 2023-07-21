e63d669e54fa4fad7444e863ddb9bff32bd6051a
Minsu Kim <qmdlrhdfyd@naver.com>
2023-07-21 16:31:33 +0900

    Implement: Naver Login

--- app/src/main/java/com/example/chatapp/GlobalApplication.kt
+++ app/src/main/java/com/example/chatapp/GlobalApplication.kt
@@ -2,10 +2,12 @@ package com.example.chatapp
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
+import com.navercorp.nid.NaverIdLoginSDK
 
 class GlobalApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, getString(R.string.kakao_native_key))
+        NaverIdLoginSDK.initialize(this, getString(R.string.naver_client_id), getString(R.string.naver_client_secret), getString(R.string.naver_client_name))
     }
 }
--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -3,27 +3,25 @@ package com.example.chatapp
 import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.ImageView
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.OAuthLoginCallback
 
-class MainActivity : ComponentActivity(), View.OnClickListener {
+class MainActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var kakaoLoginButton = findViewById<ImageView>(R.id.kakaoLoginBtn)
-        kakaoLoginButton.setOnClickListener(this)
+        var kakaoLoginButton = findViewById<ImageButton>(R.id.kakaoLoginBtn)
+        var naverLoginButton = findViewById<ImageButton>(R.id.naverLoginBtn)
 
-
-    }
-
-    override fun onClick(v: View?){
-        if(v != null){
+        kakaoLoginButton.setOnClickListener{
             val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
                 if (error != null) {
                     Log.e(TAG, "카카오계정으로 로그인 실패", error)
@@ -33,8 +31,8 @@ class MainActivity : ComponentActivity(), View.OnClickListener {
             }
 
             // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
-            if (UserApiClient.instance.isKakaoTalkLoginAvailable(v.context)) {
-                UserApiClient.instance.loginWithKakaoTalk(v.context) { token, error ->
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(kakaoLoginButton.context)) {
+                UserApiClient.instance.loginWithKakaoTalk(kakaoLoginButton.context) { token, error ->
                     if (error != null) {
                         Log.e(TAG, "카카오톡으로 로그인 실패", error)
 
@@ -45,14 +43,39 @@ class MainActivity : ComponentActivity(), View.OnClickListener {
                         }
 
                         // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
-                        UserApiClient.instance.loginWithKakaoAccount(v.context, callback = callback)
+                        UserApiClient.instance.loginWithKakaoAccount(kakaoLoginButton.context, callback = callback)
                     } else if (token != null) {
                         Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
                     }
                 }
             } else {
-                UserApiClient.instance.loginWithKakaoAccount(v.context, callback = callback)
+                UserApiClient.instance.loginWithKakaoAccount(kakaoLoginButton.context, callback = callback)
             }
+        }
+
+        naverLoginButton.setOnClickListener{
+            val oauthLoginCallback = object : OAuthLoginCallback {
+                override fun onSuccess() {
+                    // 네이버 로그인 인증이 성공했을 때 수행할 코드 추가
+//                    binding.tvAccessToken.text = NaverIdLoginSDK.getAccessToken()
+//                    binding.tvRefreshToken.text = NaverIdLoginSDK.getRefreshToken()
+//                    binding.tvExpires.text = NaverIdLoginSDK.getExpiresAt().toString()
+//                    binding.tvType.text = NaverIdLoginSDK.getTokenType()
+//                    binding.tvState.text = NaverIdLoginSDK.getState().toString()
+                    Toast.makeText(naverLoginButton.context,"네이버 로그인 성공",Toast.LENGTH_SHORT).show()
+
+                }
+                override fun onFailure(httpStatus: Int, message: String) {
+                    val errorCode = NaverIdLoginSDK.getLastErrorCode().code
+                    val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
+                    Toast.makeText(naverLoginButton.context,"errorCode:$errorCode, errorDesc:$errorDescription",Toast.LENGTH_SHORT).show()
+                }
+                override fun onError(errorCode: Int, message: String) {
+                    onFailure(errorCode, message)
+                }
+            }
+
+            NaverIdLoginSDK.authenticate(naverLoginButton.context, oauthLoginCallback)
         }
     }
 }
